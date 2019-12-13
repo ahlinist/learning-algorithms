c1f442e93d3b5d015d0ef413d5f46e18e72efee4
Anton Hlinisty <anton.hlinisty@gmail.com>
2019-12-13 21:38:50 +0300

    Measure time

--- src/main/java/com/algorithms/BubbleSort.java
+++ src/main/java/com/algorithms/BubbleSort.java
@@ -6,6 +6,7 @@ public class BubbleSort {
 
     public static void main(String... args) {
         int[] array = new int[] {3,6,2,7,4,9,2,76,9,2345,4,9,0,3,-1,-5,-7,5,-98};
+        long initialTime = System.nanoTime();
 
         for (int i = 0; i < array.length -1; i++) {
             for (int j = 0; j < array.length - i - 1; j++) {
@@ -17,7 +18,9 @@ public class BubbleSort {
             }
         }
 
+        long finalTime = System.nanoTime();
         Arrays.stream(array)
                 .forEach(System.out::println);
+        System.out.println("Time consumed: " + (finalTime - initialTime));
     }
 }
--- src/main/java/com/algorithms/EuclidsAlgo.java
+++ src/main/java/com/algorithms/EuclidsAlgo.java
@@ -7,10 +7,13 @@ public class EuclidsAlgo {
     public static void main(String... args) {
         long i = 8024713123123123127l;
         long j = 9024712234234234233l;
+        long initialTime = System.nanoTime();
 
         long result = (i >= j) ? run(i, j) : run(j, i);
+        long finalTime = System.nanoTime();
         System.out.println(result);
         System.out.println("counter: " + counter);
+        System.out.println("Time consumed: " + (finalTime - initialTime));
     }
 
     private static long run(long i, long j) {
